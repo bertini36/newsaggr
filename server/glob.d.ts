a50888c3ec2cece50820dea7b1ef23b7aa706038
Alberto Pou <albertopouquiros@gmail.com>
2025-11-22 17:29:14 +0100

    refactor: remove Product Hunt integration and dayjs patch.

--- server/glob.d.ts
+++ server/glob.d.ts
@@ -4,6 +4,5 @@ declare module 'glob:./sources/{*.ts,**/index.ts}' {
   export const github: typeof import('./sources/github')
   export const groundnews: typeof import('./sources/groundnews')
   export const hackernews: typeof import('./sources/hackernews')
-  export const producthunt: typeof import('./sources/producthunt')
   export const wired: typeof import('./sources/wired')
 }
